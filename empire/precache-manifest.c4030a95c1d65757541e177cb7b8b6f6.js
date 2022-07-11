deff4c3382b13725271e439c4a7a665c1a519f74
supercw <2366921820@qq.com>
2022-07-11 16:04:01 +0800

    提交

---
+++ empire/precache-manifest.c4030a95c1d65757541e177cb7b8b6f6.js
@@ -0,0 +1,282 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "304dcbcd93105e0729f2",
+    "url": "/empire/css/app.f91ac675.css"
+  },
+  {
+    "revision": "cdd96a548b24616b29ba",
+    "url": "/empire/css/chunk-0fc76e1f.0142f1c9.css"
+  },
+  {
+    "revision": "4811d4100027c73de475",
+    "url": "/empire/css/chunk-vendors.ec8c9596.css"
+  },
+  {
+    "revision": "f36638c2135b71e5a623dca52b611173",
+    "url": "/empire/fonts/Roboto-Regular.f36638c2.ttf"
+  },
+  {
+    "revision": "aaf961d3ef37b1ca7a5a8e6f9a4be11d",
+    "url": "/empire/fonts/TTLakesNeue-Black.aaf961d3.otf"
+  },
+  {
+    "revision": "5a17438e0f430884fa0485f36c911f7e",
+    "url": "/empire/fonts/TTLakesNeue-XBold.5a17438e.otf"
+  },
+  {
+    "revision": "c30a54541dc58fcfc9863831591ce743",
+    "url": "/empire/fonts/TTLakesNeueComp-DBold.c30a5454.otf"
+  },
+  {
+    "revision": "db78313c1987161a372c727f7feb2c0c",
+    "url": "/empire/fonts/TTLakesNeueExt-Bold.db78313c.otf"
+  },
+  {
+    "revision": "ff14f5968333891da84368772e4d431a",
+    "url": "/empire/fonts/TTLakesNeueExt-XBold.ff14f596.otf"
+  },
+  {
+    "revision": "76906e3fcb40f6dcc8ea876eee035301",
+    "url": "/empire/fonts/VCR-OSD-Mono.76906e3f.ttf"
+  },
+  {
+    "revision": "2b69951192d6767a3d5515031aa129e7",
+    "url": "/empire/img/001.2b699511.png"
+  },
+  {
+    "revision": "8a8ee1f9c15506a317f75d1460f100df",
+    "url": "/empire/img/002.8a8ee1f9.png"
+  },
+  {
+    "revision": "9c505d845f2600227c5194ad4b82b205",
+    "url": "/empire/img/003.9c505d84.png"
+  },
+  {
+    "revision": "9e7607208c04b12b8498c2ab53cba112",
+    "url": "/empire/img/004.9e760720.png"
+  },
+  {
+    "revision": "1b3ffc0128d00adec60374ad38018821",
+    "url": "/empire/img/005.1b3ffc01.png"
+  },
+  {
+    "revision": "122a79d9960f43b740348e533aeab139",
+    "url": "/empire/img/007.122a79d9.png"
+  },
+  {
+    "revision": "17de827bcde9aa758f21fd72f2cad926",
+    "url": "/empire/img/008.17de827b.png"
+  },
+  {
+    "revision": "ab0949be40cffe807a6b9db58a6cfd0d",
+    "url": "/empire/img/009.ab0949be.png"
+  },
+  {
+    "revision": "16666fb2a007ec9fefdef31fe61f72cc",
+    "url": "/empire/img/1.16666fb2.png"
+  },
+  {
+    "revision": "d06e7a9f45d406ec2a7b152d88b8e1c1",
+    "url": "/empire/img/1.d06e7a9f.png"
+  },
+  {
+    "revision": "49378d0433f05e1a54dabf0ffeb9483f",
+    "url": "/empire/img/2.49378d04.png"
+  },
+  {
+    "revision": "3e8d754e9f0d9ea48dd5a5db814db25c",
+    "url": "/empire/img/3.3e8d754e.png"
+  },
+  {
+    "revision": "5cc936748c3b39b68644001d2d8666a7",
+    "url": "/empire/img/3.5cc93674.png"
+  },
+  {
+    "revision": "44a2f83eef14562975b8b0387a0622f3",
+    "url": "/empire/img/4.44a2f83e.png"
+  },
+  {
+    "revision": "6e23777a7fd298a8313d686ae2abba48",
+    "url": "/empire/img/6.6e23777a.png"
+  },
+  {
+    "revision": "1a109e17ded3a6b470eb91ac3b306f63",
+    "url": "/empire/img/about1.1a109e17.png"
+  },
+  {
+    "revision": "cf7b007c90bcd37fc80580fa1c0c1545",
+    "url": "/empire/img/about3.cf7b007c.png"
+  },
+  {
+    "revision": "b38449e8e5fe4efa518d99cd89dc95c2",
+    "url": "/empire/img/about4.b38449e8.png"
+  },
+  {
+    "revision": "7dc91e9c55c8927a0523794ddecd9793",
+    "url": "/empire/img/about5.7dc91e9c.png"
+  },
+  {
+    "revision": "30608c0e279ee96cd7d0c3e75dcacca9",
+    "url": "/empire/img/bg.30608c0e.png"
+  },
+  {
+    "revision": "8a4264950e022817f26c8c34b4f2c162",
+    "url": "/empire/img/bg.8a426495.png"
+  },
+  {
+    "revision": "8115e96464f46074c301239634ed3630",
+    "url": "/empire/img/foot1.8115e964.png"
+  },
+  {
+    "revision": "5a5003f3890b24da5b7be0a6bb0967bd",
+    "url": "/empire/img/foot2.5a5003f3.png"
+  },
+  {
+    "revision": "16399e3ed38b3d14808d9590a61f006d",
+    "url": "/empire/img/home1.16399e3e.png"
+  },
+  {
+    "revision": "e045ab02549b6729623486ce97d585b1",
+    "url": "/empire/img/home10.e045ab02.png"
+  },
+  {
+    "revision": "2718ae997c499f347602dc69fcfe268c",
+    "url": "/empire/img/home11.2718ae99.png"
+  },
+  {
+    "revision": "a8abfca38711e5cd00f854b78d5cf6f2",
+    "url": "/empire/img/home2.a8abfca3.png"
+  },
+  {
+    "revision": "a131c3117ddc4835b410c00a2bb22750",
+    "url": "/empire/img/home4.a131c311.png"
+  },
+  {
+    "revision": "4cf75fd4c53aad6d796be71ac89a3b18",
+    "url": "/empire/img/home5.4cf75fd4.png"
+  },
+  {
+    "revision": "dd38d72c7487d4e6e3e5426e9b7273df",
+    "url": "/empire/img/home6.dd38d72c.png"
+  },
+  {
+    "revision": "c8e728e69e41e433c438f9bfb1504907",
+    "url": "/empire/img/home7.c8e728e6.png"
+  },
+  {
+    "revision": "0109dba8a8fa2dcd8bbe0067c8a972e3",
+    "url": "/empire/img/home8.0109dba8.png"
+  },
+  {
+    "revision": "5324f9cdd3d517f1f619f230281b7e5a",
+    "url": "/empire/img/loading.gif"
+  },
+  {
+    "revision": "0c824055a9e9b98ce3fc1827804f5c79",
+    "url": "/empire/img/logo.0c824055.png"
+  },
+  {
+    "revision": "12df991f0ea605d6b86c1ced7cde778f",
+    "url": "/empire/img/logo.12df991f.png"
+  },
+  {
+    "revision": "601c5682a9da8746f592b341a6221f83",
+    "url": "/empire/img/nft-avatar1.601c5682.png"
+  },
+  {
+    "revision": "aa12dc20e63e738868bff979c431dc63",
+    "url": "/empire/img/nft-avatar2.aa12dc20.png"
+  },
+  {
+    "revision": "734a9a9056345663a53503590af510d0",
+    "url": "/empire/img/nft-avatar3.734a9a90.png"
+  },
+  {
+    "revision": "a6c1467fb463a55becfc1b1098577c35",
+    "url": "/empire/img/nft-avatar4.a6c1467f.png"
+  },
+  {
+    "revision": "f2a2153ca9d1bd2968652973d9eb3e34",
+    "url": "/empire/img/nft-avatar5.f2a2153c.png"
+  },
+  {
+    "revision": "61c3e3c05390b9d6fb533643ed2b5953",
+    "url": "/empire/img/nft1.61c3e3c0.png"
+  },
+  {
+    "revision": "ae17875a55576006ee3c2653d931b3e6",
+    "url": "/empire/img/nft2.ae17875a.png"
+  },
+  {
+    "revision": "32f89dc29007f1e23b4535484b9b109a",
+    "url": "/empire/img/nft3.32f89dc2.png"
+  },
+  {
+    "revision": "d8f011754c83d446815c3e03ef411de8",
+    "url": "/empire/img/nft4.d8f01175.png"
+  },
+  {
+    "revision": "547dd38ab10102e9d451bf8f72281ad2",
+    "url": "/empire/img/nft5.547dd38a.png"
+  },
+  {
+    "revision": "63d30a2d708c5982d0a56dceac54bc8a",
+    "url": "/empire/img/nft6.63d30a2d.png"
+  },
+  {
+    "revision": "7efbf896d6270c46c93bed5f710b47a4",
+    "url": "/empire/img/nft7.7efbf896.png"
+  },
+  {
+    "revision": "4109f166907877bae7acb8ec16d5e1e9",
+    "url": "/empire/img/nft8.4109f166.png"
+  },
+  {
+    "revision": "020ad3dac12b3a9341e751be95e80665",
+    "url": "/empire/img/universe1.020ad3da.png"
+  },
+  {
+    "revision": "8606ceb1d8b306a48d85c8d1b1d55640",
+    "url": "/empire/img/universe2.8606ceb1.png"
+  },
+  {
+    "revision": "fa2aff690f182a355a071e7246956861",
+    "url": "/empire/img/universe3.fa2aff69.png"
+  },
+  {
+    "revision": "9adcb27dae528de9ffee3ff888bc44a1",
+    "url": "/empire/img/universe4.9adcb27d.png"
+  },
+  {
+    "revision": "0f9074b023e268320f6c10e2462a8258",
+    "url": "/empire/img/universe5.0f9074b0.png"
+  },
+  {
+    "revision": "1f83efe49b8714bda62a554382f31a14",
+    "url": "/empire/img/universe6.1f83efe4.png"
+  },
+  {
+    "revision": "1782341da1c548e09c8f785d0341d4ff",
+    "url": "/empire/index.html"
+  },
+  {
+    "revision": "304dcbcd93105e0729f2",
+    "url": "/empire/js/app.9b2d2f2f.js"
+  },
+  {
+    "revision": "cdd96a548b24616b29ba",
+    "url": "/empire/js/chunk-0fc76e1f.22b77b55.js"
+  },
+  {
+    "revision": "4811d4100027c73de475",
+    "url": "/empire/js/chunk-vendors.e866004a.js"
+  },
+  {
+    "revision": "1aafb06cf2d99f25754b6797bf287d65",
+    "url": "/empire/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/empire/robots.txt"
+  }
+]);
